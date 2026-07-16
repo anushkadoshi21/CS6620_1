c2f2ba22b8e7e1fadcee40ba26f809ac2e709aa9
anushkadoshi21 <anushkadoshi21@gmail.com>
2026-07-16 01:36:09 -0400

    Updating models

--- app/models/client.py
+++ app/models/client.py
@@ -8,12 +8,10 @@ from pydantic import BaseModel, Field
 class ClientBase(BaseModel):
     name: str
     organization_type: str
-    total_headcount: int = Field(ge=0)
-    joined_at: datetime
-    total_valuation: float = Field(ge=0)
-    billed_amount: float = Field(ge=0)
-    amount_paid: float = Field(ge=0)
-    latest_transaction_date: Optional[datetime] = None
+    total_headcount: int = Field(default=0, ge=0)
+    total_valuation: float = Field(default=0, ge=0)
+    billed_amount: float = Field(default=0, ge=0)
+    amount_paid: float = Field(default=0, ge=0)
 
 
 class ClientCreate(ClientBase):
@@ -21,15 +19,12 @@ class ClientCreate(ClientBase):
 
 
 class ClientUpdate(BaseModel):
-    name: Optional[str] = None
     organization_type: Optional[str] = None
     total_headcount: Optional[int] = Field(default=None, ge=0)
-    joined_at: Optional[datetime] = None
     total_valuation: Optional[float] = Field(default=None, ge=0)
     billed_amount: Optional[float] = Field(default=None, ge=0)
     amount_paid: Optional[float] = Field(default=None, ge=0)
-    latest_transaction_date: Optional[datetime] = None
 
 
 class ClientResponse(ClientBase):
-    id: UUID = Field(default_factory=uuid4)
+    pass
