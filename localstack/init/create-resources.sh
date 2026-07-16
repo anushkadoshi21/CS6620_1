54a092582c22dfd68653247cdca68d24d1f4ffa0
anushkadoshi21 <anushkadoshi21@gmail.com>
2026-07-16 01:35:45 -0400

    adding init resource sh files for s3 & dynamodb as soon as localstack isn initiated

---
+++ localstack/init/create-resources.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Creating DynamoDB table 'clients'..."
+awslocal dynamodb create-table \
+  --table-name clients \
+  --attribute-definitions AttributeName=name,AttributeType=S \
+  --key-schema AttributeName=name,KeyType=HASH \
+  --billing-mode PAY_PER_REQUEST
+
+echo "Creating S3 bucket 'clients'..."
+awslocal s3 mb s3://clients
+
+echo "Init complete."chmod +x localstack/init/01-create-resources.sh
